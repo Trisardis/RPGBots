9a56dfb40220e569d85d17a4adc54628796fb0ab
scoop267 <87953646+Trisardis@users.noreply.github.com>
2022-07-01 20:35:14 -0400

    Created a Dialog Controller

---
+++ RPGBots/Assets/Scripts/UI/DialogController.cs
@@ -0,0 +1,48 @@
+﻿using Ink.Runtime;
+using System;
+using System.Text;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogController : MonoBehaviour
+{
+    [SerializeField] TMP_Text _storyText;
+    [SerializeField] Button[] _choiceButtons;
+    [SerializeField] TextAsset _dialog;
+
+    Story _story;
+
+    [ContextMenu("Start Dialog")]
+    public void StartDialog()
+    {
+        _story = new Story(_dialog.text);
+        RefreshView();
+    }
+
+    void RefreshView()
+    {
+        StringBuilder storyTextBuilder = new StringBuilder();
+        while (_story.canContinue)
+            storyTextBuilder.AppendLine(_story.Continue());
+
+        _storyText.SetText(storyTextBuilder);
+
+        for (int i = 0; i < _choiceButtons.Length; i++)
+        {
+            var button = _choiceButtons[i];
+            button.gameObject.SetActive(i < _story.currentChoices.Count);
+            button.onClick.RemoveAllListeners();
+            if (i < _story.currentChoices.Count)
+            {
+                var choice = _story.currentChoices[i];
+                button.GetComponentInChildren<TMP_Text>().SetText(choice.text);
+                button.onClick.AddListener(() =>
+                {
+                    _story.ChooseChoiceIndex(choice.index);
+                    RefreshView();
+                });
+            }
+        }
+    }
+}
