b278bc97db44c5077448d1d7201489cbf5e1c613
scoop267 <87953646+Trisardis@users.noreply.github.com>
2022-07-02 18:09:56 -0400

    Added a Dialog Giver

---
+++ RPGBots/Assets/Scripts/DialogGiver.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogGiver : MonoBehaviour
+{
+    [SerializeField] TextAsset _dialog;
+
+    void OnTriggerEnter(Collider other)
+    {
+        var player = other.GetComponent<ThirdPersonMover>();
+
+        if (player != null)
+        {
+            FindObjectOfType<DialogController>().StartDialog(_dialog);
+            transform.LookAt(player.transform);
+        }
+    }
+}
--- RPGBots/Assets/Scripts/UI/DialogController.cs
+++ RPGBots/Assets/Scripts/UI/DialogController.cs
@@ -9,14 +9,13 @@ public class DialogController : MonoBehaviour
 {
     [SerializeField] TMP_Text _storyText;
     [SerializeField] Button[] _choiceButtons;
-    [SerializeField] TextAsset _dialog;
 
     Story _story;
 
     [ContextMenu("Start Dialog")]
-    public void StartDialog()
+    public void StartDialog(TextAsset dialog)
     {
-        _story = new Story(_dialog.text);
+        _story = new Story(dialog.text);
         RefreshView();
     }
 
