9212e524f84e612824e4324eea1504fe31c90826
scoop267 <87953646+Trisardis@users.noreply.github.com>
2022-07-14 16:08:45 -0400

    Created a Game Event

---
+++ RPGBots/Assets/Scripts/GameEvents/GameEvent.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Game Event")]
+
+public class GameEvent : ScriptableObject
+{
+    HashSet<GameEventListener> _gameEventListeners = new HashSet<GameEventListener>();
+
+    public void Register(GameEventListener gameEventListener) => _gameEventListeners.Add(gameEventListener);
+    public void Deregister(GameEventListener gameEventListener) => _gameEventListeners.Remove(gameEventListener);
+
+    [ContextMenu(itemName:"Invoke")]
+    public void Invoke()
+    {
+        foreach (var gameEventListener in _gameEventListeners)
+        {
+            gameEventListener.RaiseEvent();
+        }
+    }
+}
+
+
---
+++ RPGBots/Assets/Scripts/GameEvents/GameEventListener.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class GameEventListener : MonoBehaviour
+{
+    [SerializeField] UnityEvent _unityEvent;
+    [SerializeField] GameEvent _gameEvent;
+    void Awake() => _gameEvent.Register(gameEventListener: this);
+    void OnDisable() => _gameEvent.Deregister(gameEventListener: this);
+    public void RaiseEvent() => _unityEvent.Invoke();
+}
